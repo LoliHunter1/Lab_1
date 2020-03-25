7c652b42bce984d6e9608a412051157aa2ce53ed
LoliHunter1 <47854086+LoliHunter1@users.noreply.github.com>
2020-03-25 13:46:46 +0300

    Add files via upload

---
+++ Лаб1/Лаб1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+       
+        static void Main()
+        {
+            /*Zad 1
+            int a, b;
+            Console.WriteLine("Введите число а");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("Введите число b");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("b = " + b);
+            int s = a + b;
+            Console.WriteLine("{0} + {1} = {2} ", a , b, s);*/
+            /*Zad 2
+            int a, b;
+            Console.WriteLine("Введите число а");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("Введите число b");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("{0} + {1} = {1} + {0}", a, b);*/
+            /*Zad 3
+            int a, b, c, s;
+            Console.WriteLine("Введите число а");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("Введите число b");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("Введите число c");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine("c = " + c);
+            s = a + b + c;
+            Console.WriteLine("{0} + {1} + {2} = {3}", a, b, c, s);*/
+            /*Zad 4
+            double a, b, s;
+            Console.WriteLine("Введите число а");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("a = ", a);
+            Console.WriteLine("Введите число b");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("b = ", b);
+            s = a * b;
+            Console.WriteLine("{0:##.#} * {1:##.#} = {2:##.#} ", a, b, s);*/
+            /*Zad 5*/
+            double a, b, s;
+            Console.WriteLine("Введите число а");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("a = ", a);
+            Console.WriteLine("Введите число b");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("b = ", b);
+            s = a / b;
+            Console.WriteLine("{0:#.###} / {1:#.###} = {2:#.###} ", a, b, s);
+            /*Zad 6
+            double a, b, c;
+            Console.WriteLine("Введите число а");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("Введите число b");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("Введите число c");
+            c = double.Parse(Console.ReadLine());
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("<{0:#.##} + {1:#.##}> + {2:#.##} = {0:#.##} + <{1:#.##} + {2:#.##}>", a, b, c);*/
+        }
+
+
+    }
+}
